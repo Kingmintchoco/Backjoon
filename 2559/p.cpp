33126e4e76e20851cb13eaf283b36d27d958b8c6
Kingmintchoco <jrkr7@naver.com>
2022-05-14 16:13:27 +0900

    gitignore

---
+++ 2559/p.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void init(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+int main(){
+    init();
+
+    freopen("p.txt", "rt", stdin);
+}
